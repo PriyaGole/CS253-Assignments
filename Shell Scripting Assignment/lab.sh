1cef3aa0eb8838f0ab37deec4baafd8b17c4b988
Priya Gole <86489225+PriyaGole@users.noreply.github.com>
2022-07-01 12:51:54 +0530

    Update lab.sh

--- Shell Scripting Assignment/lab.sh
+++ Shell Scripting Assignment/lab.sh
@@ -1,6 +1,4 @@
-# README: 
-# To run the code in terminal: go to the file directory and run the .sh script with 2 input arguments:
-# $ ./lab.sh input.csv output.txt
+
 
 #Q1
 if [ "$#" -ne 2 ] ; then
